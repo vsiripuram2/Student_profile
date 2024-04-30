2c145c8c9f56f71d9d8e6bfd39e979574312a9df
sravan <sravancopy2811@gmail.com>
2024-04-30 07:06:49 +0530

    added log

--- student-service-frontend/src/Utils/AuthHeader.js
+++ student-service-frontend/src/Utils/AuthHeader.js
@@ -2,6 +2,7 @@ import React from "react";
 
 const jwtToken = localStorage.getItem("jwtToken");
 const AuthHeader = () => {
+  console.log('jwttoken',jwtToken);
   if (jwtToken) {
     return { Authorization: "Bearer " + jwtToken };
   }else{
